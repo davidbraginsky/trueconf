d829bfd6f5295aa0504fb67e6f0708bccc7e0f99
David <braginsky.david@yandex.ru>
2022-06-21 14:53:22 +0300

    добавил базовую структуру с 5 этажами и 1 шахтой лифта

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="container">
+    <div class="rows">
+      <div class="row"></div>
+      <div class="row"></div>
+      <div class="row"></div>
+      <div class="row"></div>
+      <div class="row"></div>
+    </div>
+    <div class="line"></div>
   </div>
 </template>
 
@@ -16,5 +24,27 @@ export default {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+}
+
+.container {
+  position: relative;
+}
+
+body {
+  background: rgb(212, 212, 212);
+}
+
+.row {
+  border: 1px solid black;
+  height: 100px;
+}
+
+.line {
+  height: 500px;
+  width: 100px;
+  border: 1px solid red;
+  position: absolute;
+  top: 0;
+  left: 0;
 }
 </style>
