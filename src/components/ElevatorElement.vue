8279006e3ca4abadc82e52c74c63a2ab0ad3b523
David <braginsky.david@yandex.ru>
2022-06-21 17:11:49 +0300

    разбил код на компоненты

---
+++ src/components/ElevatorElement.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="elevator" ref="elevatorRef"></div>
+</template>
+
+<script setup>
+import { ref, defineExpose } from "vue";
+
+const elevatorRef = ref(null);
+
+defineExpose({
+  elevatorRef,
+});
+</script>
+
+<style>
+.elevator {
+  height: 100px;
+  width: 100px;
+  background: goldenrod;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+}
+</style>
